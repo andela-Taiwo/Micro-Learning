de59e166f1214acedfe5be0e38fe15ec337d6cc2
SOKUNBI TAIWO <sokunbitaiwo82@gmail.com>
2018-06-27 16:04:33 +0100

    chore(host app) (#4)
    add config.ru to launch the app
    
    [Finishes -#158580023]

--- app/app.rb
+++ app/app.rb
@@ -2,3 +2,9 @@ require 'sinatra'
 require 'sinatra/activerecord'
 
 set :database_file, 'config/database.yml'
+
+class App < Sinatra::Base
+  get '/' do
+    'Welcome to Life Long Learner!'
+  end
+end
