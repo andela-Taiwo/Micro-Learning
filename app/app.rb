e4567d0ee179d02ede50bf6d8c77d3e858cbf5a7
SOKUNBI TAIWO <sokunbitaiwo82@gmail.com>
2018-06-27 17:01:21 +0100

    chore(add .env) (#5)
    add .env file
    
    [Finishes #158580027]

--- app/app.rb
+++ app/app.rb
@@ -1,7 +1,11 @@
 require 'sinatra'
+require 'dotenv/load'
 require 'sinatra/activerecord'
 
 set :database_file, 'config/database.yml'
+
+ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+
 
 class App < Sinatra::Base
   get '/' do
